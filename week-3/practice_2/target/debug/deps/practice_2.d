3d97859900c08caf86b0f99647c9d033b5f20177
maridiyahdabiri <maridiyah.dabiri@edu.ng>
2026-03-16 12:08:28 +0100

    (practice_2)

---
+++ week-3/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\Documents\maridiyahdabiri-m.dabiriCOS102\week-3\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+C:\Users\LENOVO\Documents\maridiyahdabiri-m.dabiriCOS102\week-3\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\LENOVO\Documents\maridiyahdabiri-m.dabiriCOS102\week-3\practice_2\target\debug\practice_2.exe: C:\Users\LENOVO\Documents\maridiyahdabiri-m.dabiriCOS102\week-3\practice_2\src\main.rs
